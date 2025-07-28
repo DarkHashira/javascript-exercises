25e9d683daea5d8f37e1622b2a0ee394ade03f01
Arnab Das <darnab628@gmail.com>
2025-07-28 13:30:53 +0530

    09 all test passed. Added non-int check at last

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(first,last) {
+    if(!Number.isInteger(first) || !Number.isInteger(last)){return "ERROR"}
+    if(first<0 || last <0){return "ERROR"}
+    if(first > last)
+    {
+        let temp=first;
+        first = last;
+        last = temp;
+    }
+    
+    let total=0
+    for(let i=first ; i<=last ; i++)
+    {
+        total+=i;
+    }
+    return total;
 };
 
 // Do not edit below this line
