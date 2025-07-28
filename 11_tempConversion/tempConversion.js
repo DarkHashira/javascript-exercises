665abf05d99a8bbf5664b4b25c9029d33ef599cf
Arnab Das <darnab628@gmail.com>
2025-07-29 01:46:30 +0530

    Test passed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -22,7 +22,13 @@
 // console.log(convertToFahrenheit(32))
 // console.log(convertToFahrenheit(-100))
 
+const convertToCelsius = function (temp) {
+  return Math.round((temp - 32) * (5 / 9) * 10) / 10;
+};
 
+const convertToFahrenheit = function (temp) {
+  return Math.round(((temp * 9) / 5 + 32) * 10) / 10;
+};
 
 // Do not edit below this line
 module.exports = {
