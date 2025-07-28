51b30ea8a8bcbf975fa9e56fdd122925c795c91c
Arnab Das <darnab628@gmail.com>
2025-07-29 01:44:41 +0530

    Done my code , desired output still not passing the tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,28 @@
-const convertToCelsius = function() {
-};
+// const convertToCelsius = function(temp) {
+//   let cToF = (temp * (9/5)) + 32
+//   if (Number.isInteger(cToF)) {
+//     return cToF
+//   }
+//   else{
+//     return parseFloat(cToF.toFixed(1))
+//   }
+// };
 
-const convertToFahrenheit = function() {
-};
+// const convertToFahrenheit = function(temp) {
+//   let fToC = (temp - 32) * (5/9)
+//   if (Number.isInteger(fToC)) {
+//     return fToC
+//   }
+//   else{
+//     return parseFloat(fToC.toFixed(1))
+//   }
+// };
+
+// console.log(convertToCelsius(0))
+// console.log(convertToFahrenheit(32))
+// console.log(convertToFahrenheit(-100))
+
+
 
 // Do not edit below this line
 module.exports = {
