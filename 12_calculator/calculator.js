a4b4e005485807c3ccdcdd23724062e295cb0a57
Arnab Das <darnab628@gmail.com>
2025-08-10 00:00:11 +0530

    all test passed calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(one,two) {
+	return one+two;
 };
 
-const subtract = function() {
-	
+const subtract = function(one,two) {
+	return one-two;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total,number)=> total+=number,0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total,num)=> total*=num,1)
 };
 
-const power = function() {
-	
+const power = function(num,pow) {
+	return num**pow
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if(num===1)
+    {
+      return 1
+    }
+    else if(num===0){
+      return 1
+    }
+    else{
+      return num*factorial(num-1)
+    }
+  
 };
 
 // Do not edit below this line
