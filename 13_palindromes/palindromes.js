e4aade45c8fc1b367325fd739b23798ad72e75ed
Arnab Das <darnab628@gmail.com>
2025-08-10 00:15:13 +0530

    problem 13 all test passed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890"
+    const filteredString = string.
+    toLowerCase()
+    .split("")
+    .filter((characters)=> alphanumerical.includes(characters))
+    .join('')
 
+    const reverse = filteredString.split("").reverse().join("")
+
+    return filteredString===reverse
+
+    
 };
 
 // Do not edit below this line
